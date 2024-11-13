9a8c81d5e4d6dd0d6f53875d4b03601e79d6ec33
AnshulJethva10 <anshul.jethva.25@gmail.com>
2024-11-13 15:55:10 +0530

    Adding the complete header files

---
+++ Networking.h
@@ -0,0 +1,11 @@
+/*
+ This header file contains all of the components for the Networking sub-module.
+ */
+
+#ifndef Networking_h
+#define Networking_h
+
+#include "Networking/Nodes/Server.h"
+#include "Networking/Nodes/Client.h"
+
+#endif /* Networking_h */
---
+++ peerlib.h
@@ -0,0 +1,7 @@
+#ifndef peerlib_h
+#define peerlib_h
+
+#include "DataStructures.h"
+#include "Networking.h"
+
+#endif /* libeom_h */
