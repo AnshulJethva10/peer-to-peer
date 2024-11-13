459f0b95d1ec0ac8bc6aa04b1079116c5777a81b
AnshulJethva10 <anshul.jethva.25@gmail.com>
2024-11-13 15:40:10 +0530

    Completed the code of Server.h and Server.c

---
+++ Networking/Nodes/Server.c
@@ -0,0 +1,61 @@
+/*
+ The Server struct is used as the basis for nodes that need to operate as servers.
+ Generically, the server connects to the network and listens on a given port.
+ */
+
+#include "Server.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void register_routes_server(struct Server *server, char *(*route_function)(void *arg), char *path);
+
+struct Server server_constructor(int domain, int service, int protocol, unsigned long interface, int port, int backlog)
+{
+    struct Server server;
+    // define the basic parameter of the server
+    server.domain = domain;
+    server.service = service;
+    server.protocol = protocol;
+    server.interface = interface;
+    server.port = port;
+    server.backlog = backlog;
+    // use the aforementioned parameters to construct the server's address
+    server.address.sin_family = domain;
+    server.address.sin_port = port;
+    server.address.sin_addr.s_addr = htonl(interface);
+    // create a socket for the server
+    server.socket = socket(domain, service, protocol);
+    // initialize the dictionary
+    server.routes = dictionary_constructor(compare_string_keys);
+
+    server.register_routes = register_routes_server;
+
+    // confirm the connection was successful
+    if (server.socket == 0)
+    {
+        perror("failed to connect socket...\n");
+        exit(1);
+    }
+    // attempt to bind the socket to the network
+    if ((bind(server.socket, (struct sockaddr *)&server.address, sizeof(server.address))) < 0)
+    {
+        perror("failed to bind socket...\n");
+        exit(1);
+    }
+    // start listening on the network
+    if ((listen(server.socket, server.backlog)) < 0)
+    {
+        perror("failed to start listening...\n");
+        exit(1);
+    }
+    return server;
+}
+
+void register_routes_server(struct Server server, char * (*route_function)(void *arg), char *path)
+{
+    struct ServerRoute route;
+    route.route_function = route_function;
+    server.routes.insert(&server.routes, path, sizeof(char[strlen(path)]), &route, sizeof(route));
+}
--- Networking/Nodes/Server.h
+++ Networking/Nodes/Server.h
@@ -6,4 +6,32 @@
 #ifndef Server_h
 #define Server_h
 
-#include
+#include "../../DataStructures/Dictionary/Dictionary.h"
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+struct Server
+{
+    int domain;
+    int service;
+    int protocol;
+    unsigned long interface;
+    int port;
+    int backlog;
+    struct sockaddr_in address;
+    int socket;
+
+    struct Dictionary routes;
+
+    void (*register_routes)(struct Server *server, char *(*route_function)(void *arg), char *path);
+};
+
+struct ServerRoute
+{
+    char * (*route_function)(void *arg);
+};
+
+struct Server server_constructor(int domain, int service, int protocol, unsigned long interface, int port, int backlog);
+
+#endif /* Server.h */
